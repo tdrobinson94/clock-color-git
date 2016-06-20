a648fc63d969edc43ad33067f71ba04a9104d91a
tdrobinson94 <tdrobinson94@gmail.com>
2016-06-20 16:35:03 -0400

    added 0s before single digit numbers

--- main.js
+++ main.js
@@ -8,8 +8,21 @@ var justSeconds = clock.getSeconds();
 if (justHours > 12){
   justHours = justHours - 12;
 }
+if(justHours < 10){
+  justHours = "0" + justHours;
+}
+if(justMinutes < 10){
+  justMinutes = "0" + justMinutes;
+}
+if(justSeconds < 10){
+  justSeconds = "0" + justSeconds;
+}
 
 var time = document.querySelector('.clock');
+var time2 = document.querySelector('.clock2');
+
 time.innerHTML = (justHours + ":" + justMinutes + ":" + justSeconds)
+time2.innerHTML = ("#" + justHours + justMinutes + justSeconds)
+
 };
 var intervalID = window.setInterval(timer, 1000);
