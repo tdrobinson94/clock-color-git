762548f3f899e0fa23e40896723f38f2582bbc50
tdrobinson94 <tdrobinson94@gmail.com>
2016-06-20 16:17:31 -0400

    changed hours to normal time

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ var justHours = clock.getHours();
 var justMinutes = clock.getMinutes();
 var justSeconds = clock.getSeconds();
 
+if (justHours > 12){
+  justHours = justHours - 12;
+}
+
 var time = document.querySelector('.clock');
 time.innerHTML = (justHours + ":" + justMinutes + ":" + justSeconds)
-console.log('hey')
 };
 var intervalID = window.setInterval(timer, 1000);
